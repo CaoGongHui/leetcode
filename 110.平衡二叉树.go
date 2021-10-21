6317059cda20c3966db3a752ce07d60fa25d349d
CaoGongHui <847781669@qq.com>
2021-10-21 14:36:54 +0800

    添加110平衡二叉树的go题解

--- 110.平衡二叉树.go
+++ 110.平衡二叉树.go
@@ -65,7 +65,33 @@
  * }
  */
 func isBalanced(root *TreeNode) bool {
-
+	return height(root) >= 0
+}
+func height(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftHeight := height(root.Left)
+	if leftHeight == -1 {
+		return -1
+	}
+	rightHeight := height(root.Right)
+	if rightHeight == -1 || abs(leftHeight-rightHeight) > 1 {
+		return -1
+	}
+	return max(leftHeight, rightHeight) + 1
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+	return x
 }
 
 // @lc code=end
