bd3afb70f1332b4a0e6318b0bbc42f3c9162ce6b
CaoGongHui <847781669@qq.com>
2021-10-28 16:40:50 +0800

    添加另一棵子树的golang暴力深度优先题解

---
+++ 572.另一棵树的子树.go
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=golang
+ *
+ * [572] 另一棵树的子树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	if isSame(root, subRoot) {
+		return true
+	}
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+
+}
+func isSame(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil && subRoot == nil {
+		return true
+	}
+	if root == nil || subRoot == nil {
+		return false
+	}
+	if root.Val != subRoot.Val {
+		return false
+	}
+	return isSame(root.Left, subRoot.Left) && isSame(root.Right, subRoot.Right)
+}
+
+// @lc code=end
