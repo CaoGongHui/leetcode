e4230b31ccabceeb81fda1e3e62f530210b07781
CaoGongHui <847781669@qq.com>
2021-11-09 10:36:27 +0800

    添加二叉搜索树的第k小的值的注释

--- 230.二叉搜索树中第k小的元素.go
+++ 230.二叉搜索树中第k小的元素.go
@@ -17,12 +17,17 @@ func kthSmallest(root *TreeNode, k int) int {
 	if root == nil {
 		return 0
 	}
+	// 统计左子树的节点数
 	leftCount := count(root.Left)
 	if leftCount+1 == k {
+		//左子树的节点数加1 等于k 那么root就是第k小的元素
 		return root.Val
 	} else if leftCount >= k {
+		// 左子树的节点数大于等于k 那么在左子树中找第k小的元素
 		return kthSmallest(root.Left, k)
 	} else {
+		// 左子树的节点数小于k 那么在右子树中找第k小的元素
+		// 需要找的个数就是k减去左子树的节点数加上1 这个1 就是root节点
 		return kthSmallest(root.Right, k-leftCount-1)
 	}
 }
