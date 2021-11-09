8c1a22c201093bbada3a93f63f7b04c4258eca30
CaoGongHui <847781669@qq.com>
2021-11-09 11:30:55 +0800

    添加二叉搜索树转换为累加树的golang题解

---
+++ 538.把二叉搜索树转换为累加树.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=golang
+ *
+ * [538] 把二叉搜索树转换为累加树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func convertBST(root *TreeNode) *TreeNode {
+	sum := 0
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Right)
+		node.Val += sum
+		sum = node.Val
+		dfs(node.Left)
+	}
+	dfs(root)
+	return root
+}
+
+// @lc code=end
