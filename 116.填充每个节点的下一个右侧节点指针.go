6e5d98ebddec3ce278d752191087669f98f65640
CaoGongHui <847781669@qq.com>
2021-11-24 11:34:18 +0800

    添加填充每个节点的下一个右侧节点指针的golang题解

---
+++ 116.填充每个节点的下一个右侧节点指针.go
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=golang
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Left *Node
+ *     Right *Node
+ *     Next *Node
+ * }
+ */
+
+// func connect(root *Node) *Node {
+// 	if root == nil {
+// 		return nil
+// 	}
+// 	queue := []*Node{root}
+// 	for len(queue) > 0 {
+// 		size := len(queue)
+// 		for i := 0; i < size; i++ {
+// 			node := queue[0]
+// 			queue = queue[1:]
+// 			if i < size-1 {
+// 				node.Next = queue[0]
+// 			} else {
+// 				node.Next = nil
+// 			}
+// 			if node.Left != nil {
+// 				queue = append(queue, node.Left)
+// 			}
+// 			if node.Right != nil {
+// 				queue = append(queue, node.Right)
+// 			}
+// 		}
+// 	}
+// 	return root
+// }
+
+// func connect(root *Node) *Node {
+// 	if root == nil {
+// 		return nil
+// 	}
+// 	queue := []*Node{root}
+// 	for len(queue) > 0 {
+// 		tmp := queue
+// 		size := len(tmp)
+// 		queue = nil
+// 		for i, node := range tmp {
+// 			if i+1 < size {
+// 				node.Next = tmp[i+1]
+// 			}
+// 			if node.Left != nil {
+// 				queue = append(queue, node.Left)
+// 			}
+// 			if node.Right != nil {
+// 				queue = append(queue, node.Right)
+// 			}
+// 		}
+// 	}
+// 	return root
+// }
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	for left_most := root; left_most.Left != nil; left_most = left_most.Left {
+		for node := left_most; node != nil; node = node.Next {
+			node.Left.Next = node.Right
+			if node.Next != nil {
+				node.Right.Next = node.Next.Left
+			}
+		}
+	}
+	return root
+}
+
+// @lc code=end
