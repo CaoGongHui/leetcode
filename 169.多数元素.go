93994a85fe1ceba437a2a3de4425c63d6b145bb4
CaoGongHui <847781669@qq.com>
2021-12-22 11:49:36 +0800

    添加多数元素的摩尔投票法

--- 169.多数元素.go
+++ 169.多数元素.go
@@ -14,14 +14,28 @@ func majorityElement(nums []int) int {
 	// return nums[len(nums)/2]
 
 	// 哈希表
-	m := make(map[int]int)
+	// m := make(map[int]int)
+	// for _, v := range nums {
+	// 	m[v]++
+	// 	if m[v] > len(nums)/2 {
+	// 		return v
+	// 	}
+	// }
+	// return 0
+
+	//摩尔投票法
+	m, cnt := 0, 0
 	for _, v := range nums {
-		m[v]++
-		if m[v] > len(nums)/2 {
-			return v
+		if v == m {
+			cnt++
+		} else {
+			cnt--
+		}
+		if cnt < 0 {
+			m, cnt = v, 1
 		}
 	}
-	return 0
+	return m
 }
 
 // @lc code=end
