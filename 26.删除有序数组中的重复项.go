31d4f24546d40483eab81be94609b33f2486e3f5
CaoGongHui <847781669@qq.com>
2021-12-23 16:44:47 +0800

    添加删除有序数组中重复项的golang题解使用双指针法

---
+++ 26.删除有序数组中的重复项.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=golang
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+	slow, fast := 0, 1
+	for fast < len(nums) {
+		if nums[slow] != nums[fast] {
+			slow++
+			nums[slow] = nums[fast]
+		} else {
+			fast++
+		}
+	}
+	return slow + 1
+}
+
+// @lc code=end
