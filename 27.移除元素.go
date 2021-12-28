055c4642dfe6b4e069ca5d3fb67b9b5901d859a8
CaoGongHui <847781669@qq.com>
2021-12-28 16:35:23 +0800

    添加27.移除元素的golang题解

--- 27.移除元素.go
+++ 27.移除元素.go
@@ -6,13 +6,20 @@
 
 // @lc code=start
 func removeElement(nums []int, val int) int {
-	for i := len(nums) - 1; i >= 0; i-- {
-		if nums[i] == val {
-			nums = append(nums[:i], nums[i+1:]...)
+	// for i := len(nums) - 1; i >= 0; i-- {
+	// 	if nums[i] == val {
+	// 		nums = append(nums[:i], nums[i+1:]...)
+	// 	}
+	// }
+	// return len(nums)
+	i:= 0
+	for _, v := range nums {
+		if v != val {
+			nums[i] = v
+			i++
 		}
 	}
-	return len(nums)
+	return len(nums[:i])
 }
 
 // @lc code=end
-
