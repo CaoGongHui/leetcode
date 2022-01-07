9cb70b0f4044eefabf32dd0523271d28ea029d3f
CaoGongHui <847781669@qq.com>
2022-01-07 15:31:44 +0800

    添加轮转数组的第二种揭发

--- 189.轮转数组.go
+++ 189.轮转数组.go
@@ -6,17 +6,23 @@
 
 // @lc code=start
 func rotate(nums []int, k int) {
-	k = k % len(nums)
-	reverse(nums, 0, len(nums)-1)
-	reverse(nums, 0, k-1)
-	reverse(nums, k, len(nums)-1)
-}
-func reverse(nums []int, start, end int) {
-	for start < end {
-		nums[start], nums[end] = nums[end], nums[start]
-		start++
-		end--
+	newNums := make([]int, len(nums))
+	for i, v := range nums {
+		newNums[(i+k)%len(nums)] = v
 	}
+	copy(nums, newNums)
+	// k = k % len(nums)
+	// reverse(nums, 0, len(nums)-1)
+	// reverse(nums, 0, k-1)
+	// reverse(nums, k, len(nums)-1)
 }
+
+// func reverse(nums []int, start, end int) {
+// 	for start < end {
+// 		nums[start], nums[end] = nums[end], nums[start]
+// 		start++
+// 		end--
+// 	}
+// }
 
 // @lc code=end
