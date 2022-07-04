10ff2c8f959802c3ef3dfd54a94e0a982e268a09
CaoGongHui <847781669@qq.com>
2022-07-04 15:45:48 +0800

    重复练习题解

--- 116.填充每个节点的下一个右侧节点指针.go
+++ 116.填充每个节点的下一个右侧节点指针.go
@@ -41,44 +41,65 @@
 // 	return root
 // }
 
-// func connect(root *Node) *Node {
-// 	if root == nil {
-// 		return nil
-// 	}
-// 	queue := []*Node{root}
-// 	for len(queue) > 0 {
-// 		tmp := queue
-// 		size := len(tmp)
-// 		queue = nil
-// 		for i, node := range tmp {
-// 			if i+1 < size {
-// 				node.Next = tmp[i+1]
-// 			}
-// 			if node.Left != nil {
-// 				queue = append(queue, node.Left)
-// 			}
-// 			if node.Right != nil {
-// 				queue = append(queue, node.Right)
-// 			}
-// 		}
-// 	}
-// 	return root
-// }
-
 func connect(root *Node) *Node {
+	// if root == nil {
+	// 	return nil
+	// }
+	// queue := []*Node{root}
+	// for len(queue) > 0 {
+	// 	tmp := queue
+	// 	size := len(tmp)
+	// 	queue = nil
+	// 	for i, node := range tmp {
+	// 		if i+1 < size {
+	// 			node.Next = tmp[i+1]
+	// 		}
+	// 		if node.Left != nil {
+	// 			queue = append(queue, node.Left)
+	// 		}
+	// 		if node.Right != nil {
+	// 			queue = append(queue, node.Right)
+	// 		}
+	// 	}
+	// }
+	// return root
 	if root == nil {
 		return nil
 	}
-	for leftMost := root; leftMost.Left != nil; leftMost = leftMost.Left {
-		for node := leftMost; node != nil; node = node.Next {
-
-			node.Left.Next = node.Right
-			if node.Next != nil {
-				node.Right.Next = node.Next.Left
+	queue := []*Node{root}
+	for len(queue) > 0 {
+		tmp := queue
+		size := len(tmp)
+		queue = nil
+		for i, node := range tmp {
+			if i+1 < size {
+				node.Next = tmp[i+1]
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
 			}
 		}
 	}
 	return root
 }
+
+// func connect(root *Node) *Node {
+// 	if root == nil {
+// 		return nil
+// 	}
+// 	for leftMost := root; leftMost.Left != nil; leftMost = leftMost.Left {
+// 		for node := leftMost; node != nil; node = node.Next {
+
+// 			node.Left.Next = node.Right
+// 			if node.Next != nil {
+// 				node.Right.Next = node.Next.Left
+// 			}
+// 		}
+// 	}
+// 	return root
+// }
 
 // @lc code=end
