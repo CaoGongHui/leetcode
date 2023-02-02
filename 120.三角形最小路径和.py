40aa1c4df06cbecb486517269792abff953f9c2d
CaoGongHui <847781669@qq.com>
2023-02-02 11:50:55 +0800

    添加python解法

---
+++ 120.三角形最小路径和.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=120 lang=python3
+#
+# [120] 三角形最小路径和
+#
+
+# @lc code=start
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        f = [[0] * n for _ in range(n)]
+        f[0][0] = triangle[0][0]
+        for i in range(1, n):
+            f[i][0] = f[i-1][0] + triangle[i][0]
+            for j in range(1, i):
+                f[i][j] = min(f[i-1][j-1], f[i-1][j]) + triangle[i][j]
+            f[i][i] = f[i-1][i-1] + triangle[i][i]
+        return min(f[n-1])
+# @lc code=end
