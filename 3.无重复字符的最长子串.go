69e191e9eeb2c6f279d672b4f56d54b1df00183d
gonghui_cao <gonghui_cao@intsig.net>
2025-03-14 11:10:07 +0800

    添加不定长滑动窗口的新题目

--- 3.无重复字符的最长子串.go
+++ 3.无重复字符的最长子串.go
@@ -6,19 +6,32 @@
 
 // @lc code=start
 func lengthOfLongestSubstring(s string) int {
-	start, end := 0, 0
+	// start, end := 0, 0
+	// for i := 0; i < len(s); i++ {
+	// 	index := strings.Index(s[start:i], string(s[i]))
+	// 	if index == -1 {
+	// 		if i+1 > end {
+	// 			end = i + 1
+	// 		}
+	// 	} else {
+	// 		start += index + 1
+	// 		end += index + 1
+	// 	}
+	// }
+	// return end - start
+	ans := 0
+	cnt := make(map[byte]int)
+	left := 0
 	for i := 0; i < len(s); i++ {
-		index := strings.Index(s[start:i], string(s[i]))
-		if index == -1 {
-			if i+1 > end {
-				end = i + 1
-			}
-		} else {
-			start += index + 1
-			end += index + 1
+		cnt[s[i]]++
+		for cnt[s[i]] > 1 {
+			cnt[s[left]]--
+			left++
 		}
+		ans = max(ans, i-left+1)
 	}
-	return end - start
+
+	return ans
 }
 
 // func lengthOfLongestSubstring(s string) int {
